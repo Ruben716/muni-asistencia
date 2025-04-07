f8be415db28e1650d8114d58e8b422980c5845cd
crstiancg <himkaneki@gmail.com>
2025-04-07 14:40:07 -0500

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
 {
     $this->call([
-        UserSeeder::class, 
         RolesSeeder::class,
-        
+        UserSeeder::class, 
     ]);
 }
 
